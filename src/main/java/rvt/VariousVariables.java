a04e5dbeb601afe0146fc889a425f6df02b7ae8f
24DP2AEZari <a240579az@rvt.lv>
2025-09-10 13:07:14 +0300

    Izveidots uzdevums par mainīgajiem java

---
+++ src/main/java/rvt/VariousVariables.java
@@ -0,0 +1,23 @@
+package rvt;
+
+public class VariousVariables {
+    public static void main(String[] args) {
+        int counter = 9000;
+        double kg = 0.1;
+        String tractor = "Zetor";
+
+        System.out.println("Chicken: ");
+        System.out.println(counter);
+        System.out.println("Bacon (kg): ");
+        System.out.println(kg);
+        System.out.println("Tractor: ");
+        System.out.println(tractor);
+        System.out.println();
+        
+        System.out.println("And, finally a summary: ");
+        System.out.println(counter);
+        System.out.println(kg);
+        System.out.println(tractor);
+
+    }
+}
